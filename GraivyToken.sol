a777809782dc55336bd8cb5bfe473cdd052f1a21
grAIvy <joltz@tuta.io>
2017-09-01 22:13:03 -0700

    Create GraivyToken.sol

---
+++ GraivyToken.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.11;
+
+
+import './MintableToken.sol';
+
+/**
+ * @title Mintable Graivy token
+ * @dev ERC20 Token with mintable token creation
+ * @dev Issue: * https://github.com/graivy/graivy-token
+ * Based on code by OpenZeppelin: https://github.com/OpenZeppelin/zeppelin-solidity
+ */
+
+contract GraivyToken is MintableToken {
+  string public constant name = "grAIvy Tokens";
+  string public constant symbol = "GVY";
+  uint8 public constant decimals = 18;
+  string public version = 'GVY0.3.0';
+
+
+  function GraivyToken() {
+      totalSupply = 0;
+      supplyMod = 1;
+      tokenWeight = 1000000000000000;
+      tokenCap = 200000000;
+      trackerThreshold = 10000000;
+      mintMinimum = 1000000000000000;
+      claimMinimum = 1000000000000000;
+      claimLockTime = 24 hours;
+  }
+}
