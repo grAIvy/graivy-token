03eff56653317da907aeaa76005c2afc512b5095
grAIvy <joltz@tuta.io>
2017-09-09 20:40:20 -0700

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -8,7 +8,7 @@ module.exports = {
     rinkeby: {
       host: "localhost", // Connect to geth on the specified
       port: 8545,
-      from: "0x39620d5f8045bbD8D0ea777ce7fc0A308C6a0A7a", // default address to use
+      from: "your_local_address", // default address to use
       network_id: 4,
       gas: 4710388 // Gas limit used for deploys
     }
